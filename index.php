058951d760d5d960a942280f07f365ebd72a246e
toxicbloud <fanta.r55000@gmail.com>
2021-11-26 15:51:47 +0100

    ouiiiii le td est fait

--- index.php
+++ index.php
@@ -5,12 +5,37 @@ require_once('Requetes.php');
 
 use \wish\bd\connectionFactory;
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+
+
+
+$app = new \Slim\App;
+
 $db = new connectionFactory();
 $conn = $db->makeConnection();
 
 $req = new requetes();
 
-$req::listItems($conn);
-echo('<br>');
-$req::getItem($conn, 2);
+$app->get(
+    '/listes',
+    function ($rq, $rs, $args) {
+        $rs->getBody()->write("printage des liste".'<br>');
+    }
+);
+$app->get(
+    '/liste/{id}',
+    function ($rq, $rs, $args) {
+        $rs->getBody()->write("liste numero: " . $args['id'].'<br>');
+    }
+);
+$app->get(
+    '/item/{id}',
+    function ($rq, $rs, $args) use ($conn) {
+        $req=new requetes();
+        $rs->getBody()->write("item numero: " . $args['id'].'<br>'.$req::getItem($conn, $args['id']));
+    }
+);
+$app->run();
+
+
 
