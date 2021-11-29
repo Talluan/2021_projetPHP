8936086b0e20fc6b0d1402cc87fb6b22400cee61
Talluan <tmclaudel@gmail.com>
2021-11-29 17:34:36 +0100

    Update Liste.php

--- src/models/Liste.php
+++ src/models/Liste.php
@@ -1,45 +1,12 @@
 <?php
 
-namespace wish\objets;
+namespace wish\models;
 use \Exception;
 
-class Liste 
+class Liste extends \Illuminate\Database\Eloquent\Model
 {
-    private $no;
-    private $user_id;
-    private $titre;
-    private $description;
-    private $expiration;
-    private $token;
-
-    /**
-     * Constructeur de Liste
-     * @param $usr, ID de l'utilisateur
-     * @param $titre, titre de la liste
-     * @param $description, description de la liste
-     * @param $expi, date d'expiration de la liste
-     * @param $token, token de la liste
-     */
-    public function __construct($usr, $titre, $description = null, $expi = null, $token = null)
-    {
-        $this->user_id = $usr;
-        $this->titre = $titre;
-        $this->description = $description;
-        $this->expiration = $expi;
-        $this->token = $token;
-    }
-
-    /**
-     * getter des attributs de Liste
-     */
-    public function __get($name)
-    {
-        if (property_exists ($this, $name)) {
-            return $this->$name;
-        }
-        else throw new Exception("$name : invalid property");
-    }
-
-
+    protected $table = "liste";
+    protected $primaryKey = "no";
+    public $timestamps = false;
 
 }
