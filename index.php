9765b165c361a0afefb53a83419df46741e7e137
toxicbloud <fanta.r55000@gmail.com>
2021-11-29 17:50:51 +0100

    update db init

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
 
 require_once('vendor/autoload.php');
+require_once('conf/db.php');
 use \Psr\Http\Message\ServerRequestInterface as Request;
-use Illuminate\Database\Capsule\Manager as DB;
 
-$db = new DB();
-$tab = parse_ini_file('conf/db.config.ini');
-$db->addConnection([
-    'driver'    => $tab['driver'],
-    'host'      => $tab['host'],
-    'database'  => $tab['database'],
-    'username'  => $tab['username'],
-    'password'  => $tab['password'],
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-$db->setAsglobal();
-$db->bootEloquent();
+
 $app = new \Slim\App;
 
 
@@ -35,6 +22,12 @@ $app->get(
 );
 $app->get(
     '/item/{id}',
+    function ($rq, $rs, $args) {
+        $rs->getBody()->write("item numero: " . $args['id'].'<br>');
+    }
+);
+$app->get(
+    'index.php',
     function ($rq, $rs, $args) {
         $rs->getBody()->write("item numero: " . $args['id'].'<br>');
     }
