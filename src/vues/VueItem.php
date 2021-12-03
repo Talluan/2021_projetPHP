a45072f9b89d9d36e177f327b421b1d4dce380da
Maion Mathieu <mathieu.maion2@etu.univ-lorraine.fr>
2021-12-03 15:27:20 +0100

    Update VueItem.php

---
+++ src/vues/VueItem.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace wish\vues;
+use \Exception;
+
+class VueItem{
+
+    private $modele;
+
+    public function __construct($mod){
+        $this->modele = $mod;
+    }
+
+    public function render(){
+        $content = $this->htmlUnItem();
+        $html = <<<END
+        <!DOCTYPE html> <html>
+        <body>
+         <div class="content">
+            $content
+         </div>
+        </body><html>
+        END;
+
+        return $html;
+    }
+
+    public function htmlUnItem(){
+        $res ="";
+        foreach ($this->modele->items as $value) {
+            $res+="$value->nom."<br>""
+        }
+        return res;
+    }
+
+}
