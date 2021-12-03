a0e0f7462d6a506cea493dcad3bde160dc3a9088
toxicbloud <fanta.r55000@gmail.com>
2021-12-03 15:55:29 +0100

    Update VueListe.php

--- src/vues/VueListe.php
+++ src/vues/VueListe.php
@@ -1,3 +1,37 @@
 <?php
 
 namespace wish\vues;
+
+class VueListe
+{
+    private $model;
+    function __construct($liste)
+    {
+        $this->model = $liste;
+    }
+
+    public function render()
+    {
+        $content = $this->htmlListe();
+        $html = <<<END
+        <!DOCTYPE html> <html>
+        <body>
+         <div class="content">
+             <h1>
+              Affiche d'un Item ! 
+             </h1>
+            $content
+         </div>
+        </body><html>
+END;
+        return $html;
+    }
+
+    public function htmlListe(){
+        $res ="";
+        foreach ($this->model->items as $value) {
+            $res+="$value->nom.<br>";
+        }
+        return $res;
+    }
+}
