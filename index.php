87365deb486ed6cda1d8abcca050b53a62b50041
toxicbloud <fanta.r55000@gmail.com>
2021-12-03 14:14:56 +0100

    correction betise Tom

--- index.php
+++ index.php
@@ -6,20 +6,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use Illuminate\Database\Capsule\Manager as DB;
 use wish\models\Liste;
 
-$db = new DB();
-$tab = parse_ini_file('conf/db.config.ini');
-$db->addConnection([
-    'driver'    => $tab['driver'],
-    'host'      => $tab['host'],
-    'database'  => $tab['database'],
-    'username'  => $tab['username'],
-    'password'  => $tab['password'],
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-$db->setAsglobal();
-$db->bootEloquent();
 $app = new \Slim\App;
 
 
