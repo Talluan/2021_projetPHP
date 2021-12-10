92c67f5d354796b1dfa29686154689893007405b
Talluan <tmclaudel@gmail.com>
2021-12-10 15:23:22 +0100

    Update VueCreerListe.php

--- src/vues/VueCreerListe.php
+++ src/vues/VueCreerListe.php
@@ -20,18 +20,17 @@ class VueCreerListe
     public function creerListe()
     {
         $host = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST'];
-        var_dump($_SERVER);
         $res = <<<END
 
         <div class="container">
         <form action="$host/projetphp/creerliste" method="POST">
         <div class="row">
           <div class="col">
-          <input type="text" class="form-control" placeholder="Nom Liste">
+          <input type="text" name="nom" class="form-control" placeholder="Nom Liste">
           </div>
           <div class="form-group">
           <label for="exampleFormControlTextarea1">Description de la liste</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          <textarea name="descr" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         <button class="btn btn-primary" type="submit">créer</button>
      </div>
