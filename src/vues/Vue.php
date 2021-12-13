84f45fc699effdb29a24d81cb8a9d1d28c363760
toxicbloud <fanta.r55000@gmail.com>
2021-12-13 11:51:47 +0100

    Update Vue.php

--- src/vues/Vue.php
+++ src/vues/Vue.php
@@ -21,6 +21,23 @@ private $html;
            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
           </head>
          <body>
+         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<a class="navbar-brand" href="/">
+		<img src="../img/favicon.ico" alt="" width="60" height="60">
+	</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="membres.php">Listes</a>
+			</li>		
+		</ul>
+		
+	</div>
+</nav>
           $content
          </body>
         <html>
