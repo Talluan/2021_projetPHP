290fd4f81280c04a004ddb67a254383672c47063
Robin Krerowicz <krerowic4u@etu.univ-lorraine.fr>
2021-12-15 15:17:38 +0100

    href bouton listes header

--- src/vues/Vue.php
+++ src/vues/Vue.php
@@ -32,7 +32,7 @@ private $html;
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav">
 			<li class="nav-item">
-				<a class="nav-link" href="membres.php">Listes</a>
+				<a class="nav-link" href="listes">Listes</a>
 			</li>		
 		</ul>
 		
