5f10fb3167b457908dd62adf265a983c1cfe1fae
Robin Krerowicz <krerowic4u@etu.univ-lorraine.fr>
2022-01-05 14:51:17 +0100

    liens vers listes depuis vueListes

--- src/vues/VueListes.php
+++ src/vues/VueListes.php
@@ -28,13 +28,14 @@ class VueListes
             $attributListe = $liste->getAttributes();
             $titre = $attributListe['titre'];
             $msg = $attributListe['description'] . " | " . $attributListe['expiration'];
+            $listepath = $this->rq->getUri()->getBasePath() . "/liste/" . $attributListe['no'];
             $res .= <<<END
             <ul class="list-unstyled">
             <li class="media">
 END;
             $res .= <<<END
             <div class="media-body">
-                <h5 class="mt-0 mb-1">$titre</h5>
+                <a class="link-dark" href="$listepath"><h5 class="mt-0 mb-1">$titre</h5></a>
                 $msg
               </div>  
 END;
@@ -45,7 +46,8 @@ END;
                     $img = $this->rq->getUri()->getBasePath() . "/img/" . $attributItem['img'];
                     $itempath = '"'.$this->rq->getUri()->getBasePath() . "/item/" . $attributItem['id'].'"';
                     $res .= <<<END
-       <a href=$itempath><img class="mr-3 src="$img" alt="image Item" width="100" height="100"><a>
+       <a href=$itempath><img class="mr-3 bg-image hover-overlay ripple shadow-1-strong rounded"
+       data-mdb-ripple-color="light" src="$img" alt="image Item" width="100" height="100"><a>
 END;
                 }
             }
